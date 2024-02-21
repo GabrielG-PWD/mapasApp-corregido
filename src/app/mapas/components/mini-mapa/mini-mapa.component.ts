62cbdad50f34408e95b18a601940ce56041403e4
Gabriel <85773507+GabrielG-PWD@users.noreply.github.com>
2024-02-20 21:08:08 -0300

    proyecto terminado-propiedades agregado

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import * as maptilersdk from '@maptiler/sdk';
+
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
-  styles: ``
+  styles:
+    `
+  div {
+    width: 100%;
+    height: 150px;
+    margin: 0px;
+  }
+  `
 })
-export class MiniMapaComponent {
+export class MiniMapaComponent implements AfterViewInit {
+  @Input() lngLat: [number, number] = [0, 0];
+  @ViewChild('mapa') divMapa!: ElementRef
 
+  ngAfterViewInit(): void {
+    const mapa = new maptilersdk.Map({
+      container: this.divMapa.nativeElement, // container's id or the HTML element to render the map
+      style: maptilersdk.MapStyle.STREETS,
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false,
+      geolocateControl: false,
+      navigationControl: false
+    });
+
+    new maptilersdk.Marker()
+      .setLngLat(this.lngLat)
+      .addTo(mapa);
+  }
 }
--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -17,10 +17,8 @@ export class FullScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
-
     const map = new maptilersdk.Map({
-      container: 'mapa', // container's id or the HTML element to render the map
+      container: 'mapa',
       style: maptilersdk.MapStyle.STREETS,
       center: [-64.24977732345958, -31.432760569968032],
       zoom: 12
